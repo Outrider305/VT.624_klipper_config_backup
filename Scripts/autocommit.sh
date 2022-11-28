8d5d3f7fb6793b1f91e372841f5a2c279da868a4
Outrider305 <josepdr27@gmail.com>
2022-11-28 08:41:24 -0500

    Autocommit from 2022-11-28 08:41:24

--- Scripts/autocommit.sh
+++ Scripts/autocommit.sh
@@ -8,13 +8,13 @@
 config_folder=~/klipper_config
 
 ### Path to your Klipper folder, by default that is '~/klipper'
-#klipper_folder=~/klipper
+klipper_folder=~/klipper
 
 ### Path to your Moonraker folder, by default that is '~/moonraker'
-#moonraker_folder=~/moonraker
+moonraker_folder=~/moonraker
 
 ### Path to your Mainsail folder, by default that is '~/mainsail'
-#mainsail_folder=~/mainsail
+mainsail_folder=~/mainsail
 
 ### Path to your Fluidd folder, by default that is '~/fluidd'
 #fluidd_folder=~/fluidd
